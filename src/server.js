4d2b3dcd2e9bd1ff942868dbd20b8d7d45f8d2db
MASTRIO <mastrio5772@gmail.com>
2021-07-21 14:00:39 +1000

    yeah

--- src/server.js
+++ src/server.js
@@ -6,6 +6,9 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const cParser = require("./commandParser")
 
+// Variables
+let playersOnline = 0
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client/index.html');
 });
@@ -13,26 +16,28 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   // On user connection
   socket.on('connect to server', (username) => {
-    io.emit('recieve console message', username + " has connected to the server")
+    io.emit('recieve console chat message', username + " has connected to the server")
     console.log(`[UserConnect@${username}]: Connected`);
+    playersOnline++
   });
 
   // On disconnect
   socket.on('disconnect', () => {
-    io.emit('recieve console message', "A user has disconnected from the server")
+    io.emit('recieve console chat message', "A user has disconnected from the server")
     console.log("[UserDisconnect@UnknownUsername]: Disconnected");
+    playersOnline--
   });
 
   // On message sent
   socket.on('send message', (msg, username) => {
     console.log(`[UserMessage@${username}]: ` + msg)
-    io.emit('recieve user message', msg, username);
+    io.emit('recieve user chat message', msg, username);
   });
 
   // On command sent
   socket.on('run command', (command, username) => {
     var commandArgs = command.split(" ")
-    cParser.parse(commandArgs, username, socket, io)
+    cParser.parse(commandArgs, socket, io, username, playersOnline)
     console.log(`[UserCommand@${username}]: ` + command)
   });
 
